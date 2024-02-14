56ad678a314828012ca3cba1c4efeeca61ebf7cb
Austin Barrett <abarrett@rolkaloube.com>
2024-02-13 23:16:02 -0500

    wip

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import {useEventListener, useFullscreen} from "@vueuse/core"
+
+const el = ref<HTMLElement | null>(null)
+
+const { isFullscreen, enter, exit, toggle } = useFullscreen(el)
+
+useEventListener(document, 'keydown', (e) => {
+  if (e.key === 'f') toggle()
+})
+
+</script>
+
+<template>
+  <video @click="exit"></video>
+  <button v-show="!isFullscreen" @click="enter">enter fullscreen</button>
+</template>
+
+<style scoped>
+html, body, video {
+  background: black;
+}
+</style>
